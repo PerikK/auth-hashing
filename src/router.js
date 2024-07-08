7827e084e1d14f33fe95a5de5e9a5cd0e352b532
Periklis <perik76@gmail.com>
2024-07-08 15:24:29 +0300

    registter router done

--- src/router.js
+++ src/router.js
@@ -7,7 +7,34 @@ const prisma = new PrismaClient();
 const router = express.Router();
 
 router.post('/register', async (req, res) => {
+    const { username, password } = req.body
     
+    const existingUserName = await prisma.user.findFirst({
+        where: {
+            username: username
+        }
+    })
+
+    if (existingUserName) {
+        throw new Error('A user with this username already exists')
+    }
+
+    const passHash = await bcrypt.hash(password, 8)
+    console.log(passHash);
+    
+    const newUser = await prisma.user.create({
+        data: {
+            username: username,
+            password: passHash
+        }
+    })
+
+    res
+			.status(200)
+			.json({
+				username: newUser.username,
+				password: newUser.password,
+			})
 });
 
 router.post('/login', async (req, res) => {
